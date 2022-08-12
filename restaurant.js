eb483dd63b23caf696c95a388d37c9ceaca0be3e
Joaquin Arroyo <87846400+JoaPul@users.noreply.github.com>
2022-08-11 19:05:02 -0500

    feat: starting the project

---
+++ restaurant.js
@@ -0,0 +1,26 @@
+/**
+ * 1.- Esquema
+ * 2.- nombre
+ */
+
+import mongoose from "mongoose";
+
+const restaurantSchema = new mongoose.Schema({
+  address: {
+    building: String,
+    coord: [Number, Number],
+    street: String,
+    zipcode: String,
+  },
+  borough: String,
+  cuisine: String,
+  grades: [{
+    date: Date,
+    grade: String,
+    score: Number,
+  }],
+  name: String,
+  restaurant_id: String,
+});
+
+export default mongoose.model('Restaurant');
